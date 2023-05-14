e6320bb95df4cd65afa3a96f2606ca1ef2dabbfe
pascalbothner <pascal.bothner@outlook.de>
2023-05-14 17:47:59 +0200

    Initial Commit

---
+++ todo/todoApp.swift
@@ -0,0 +1,17 @@
+//
+//  todoApp.swift
+//  todo
+//
+//  Created by Pascal Bothner on 14.05.23.
+//
+
+import SwiftUI
+
+@main
+struct todoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
